ef1def8555ca3efc0603bd3c227b15cd80fa8b53
imyzgin <113423885+imyzgin@users.noreply.github.com>
2022-09-20 20:13:27 +0500

    Новая "Нужно больше функций"

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const GetRandomNumber = function (from, to) {
+const getRandomNumber = function (from, to) {
   if (from >= 0 && to >= from){
     return Math.floor(Math.random() * (to - from + 1)) + from;
   } else if (from >= 0 && from >= to) {
     return Math.floor(Math.random() * (from - to + 1)) + to;
-  } else return console.log ('Кажется вы ввели отрицательные числа')
+  } else return 'Вы ввели отрицательные числа';
 
 };
 // источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const LengthCheckOfString = function (text, max_length) {
-  if (text >= max_length) {
+const checkLengthOfString = function (text, maxLength) {
+  if (text >= maxLength) {
     return true;
   } else return false;
 }
